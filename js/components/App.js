d0cebc2d053c748824830c26dcdb196e591374d0
Sebastian Bolling <sebastian.bolling@gmail.com>
2017-07-04 22:07:25 +0200

    updated routes and added mutations

--- js/components/App.js
+++ js/components/App.js
@@ -1,3 +1,5 @@
+
+
 import React from 'react';
 import Relay from 'react-relay';
 
---
+++ js/mutations/CheckHidingSpotForTreasureMutation.js
@@ -0,0 +1,67 @@
+import Relay from 'react-relay';
+
+export default class CheckHidingSpotForTreasureMutation extends Relay.Mutation {
+  static fragments = {
+    game: () => RelayQL`
+    fragment on Game {
+      id,
+      turnsRemaining,
+    }
+    `,
+    hidingSpot: () => RelayQL`
+    fragment on HidingSpot {
+      id,
+    }
+    `,
+  };
+
+  getMutation() {
+    return Relay.QL`mutation{checkHidingSpotForTreasure}`;
+  }
+
+  getCollisionKey() {
+    return `check_${this.props.game.id}`;
+  }
+
+  getFatQuery(){
+    return Relay.QL`
+    fragment on checkHidingSpotForTreasurePayload @relay(pattern: true) {
+      hidingSpot {
+        hasBeenChecked,
+        hasTreasure,
+      },
+      game {
+        turnsRemaining,
+      },
+    }
+    `;
+  }
+
+  getConfigs() {
+    return [{
+      type: 'FIELDS_CHANGE',
+      fieldIDs: {
+      hidingSpot: this.props.hidingSpot.id,
+      game: this.props.game.id,
+    },
+  }];
+  }
+
+  getVariables() {
+    return {
+      is. this.props.hidingSpot.id,
+    };
+  }
+
+  getOptimisticRespone() {
+    return {
+      game: {
+        turnsRemaing: this.props.game.turnsRemaining - 1,
+      },
+      hidingSpot: {
+        id: this.props.hidingSpot.id,
+        hasBeenChecked: true,
+      },
+    };
+  }
+}
