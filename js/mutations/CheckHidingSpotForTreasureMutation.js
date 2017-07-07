2146f3d56537dd4fb3bfbac67e19154cece42eb7
Sebastian Bolling <sebastian.bolling@gmail.com>
2017-07-07 21:03:54 +0200

    Updated the App.js to have the gamecode

--- js/mutations/CheckHidingSpotForTreasureMutation.js
+++ js/mutations/CheckHidingSpotForTreasureMutation.js
@@ -1,63 +1,56 @@
 import Relay from 'react-relay';
 
-//Mutations and stuff
 export default class CheckHidingSpotForTreasureMutation extends Relay.Mutation {
   static fragments = {
-    game: () => RelayQL`
-    fragment on Game {
-      id,
-      turnsRemaining,
-    }
+    game: () => Relay.QL`
+      fragment on Game {
+        id,
+        turnsRemaining,
+      }
     `,
-    hidingSpot: () => RelayQL`
-    fragment on HidingSpot {
-      id,
-    }
+    hidingSpot: () => Relay.QL`
+      fragment on HidingSpot {
+        id,
+      }
     `,
   };
-
   getMutation() {
     return Relay.QL`mutation{checkHidingSpotForTreasure}`;
   }
-
   getCollisionKey() {
     return `check_${this.props.game.id}`;
   }
-
-  getFatQuery(){
+  getFatQuery() {
     return Relay.QL`
-    fragment on checkHidingSpotForTreasurePayload @relay(pattern: true) {
-      hidingSpot {
-        hasBeenChecked,
-        hasTreasure,
-      },
-      game {
-        turnsRemaining,
-      },
-    }
+      fragment on CheckHidingSpotForTreasurePayload @relay(pattern: true) {
+        hidingSpot {
+          hasBeenChecked,
+          hasTreasure,
+        },
+        game {
+          turnsRemaining,
+        },
+      }
     `;
   }
-
   getConfigs() {
     return [{
       type: 'FIELDS_CHANGE',
       fieldIDs: {
-      hidingSpot: this.props.hidingSpot.id,
-      game: this.props.game.id,
-    },
-  }];
+        hidingSpot: this.props.hidingSpot.id,
+        game: this.props.game.id,
+      },
+    }];
   }
-
   getVariables() {
     return {
-      is. this.props.hidingSpot.id,
+      id: this.props.hidingSpot.id,
     };
   }
-
-  getOptimisticRespone() {
+  getOptimisticResponse() {
     return {
       game: {
-        turnsRemaing: this.props.game.turnsRemaining - 1,
+        turnsRemaining: this.props.game.turnsRemaining - 1,
       },
       hidingSpot: {
         id: this.props.hidingSpot.id,
